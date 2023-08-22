ed04dcbac7f132e32804764fab01ff44fd525032
Jazmin Soto <jazmin.soto@utec.edu.pe>
2023-08-22 10:50:49 -0500

    Add method put

--- api_animes.py
+++ api_animes.py
@@ -15,6 +15,7 @@ def getAnime(id_Anime):
             return anime
     return "No se encontro el anime :("
 
+
 @app.route("/anime", methods= ['POST'])
 def addAnime():
     anime = { #Que solo aceptara este formato y lo que el cliente añada de mas no lo tomara en cuenta
@@ -27,9 +28,27 @@ def addAnime():
     }
     dataAnime['anime'].append(anime)
     with open(r"C:\Users\HP\Desktop\DBP\TAREAS\anime.json", 'w') as archivo: #Abriendo archivo json para escritura
-        js.dump(dataAnime, archivo)
+        js.dump(dataAnime, archivo, indent=4)
     
     return dataAnime
+
+@app.route("/anime/{<int:id_Anime>}", methods=['PUT'])
+def UpdateAnime(id_Anime):
+    for anime in dataAnime['anime']:
+        if anime['id'] == id_Anime:
+            #anime['id'] = request.json['price']
+            anime['titulo'] = request.json['titulo']
+            anime['puntaje'] = request.json['puntaje']
+            anime['tipo'] = request.json['tipo']
+            anime['season'] = request.json['season']
+            anime['generos'] = request.json['generos']
+        
+            with open(r"C:\Users\HP\Desktop\DBP\TAREAS\anime.json", "w") as json_file:
+                js.dump(dataAnime, json_file, indent=4)
+            return dataAnime
+    return "No se encontro el anime :("
+
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
