764ce4f3f848bff9f1673a309227779a5310cceb
Jazmin Soto <jazmin.soto@utec.edu.pe>
2023-08-21 22:51:09 -0500

    Add method get (solo muestra el json de un anime)

--- api_animes.py
+++ api_animes.py
@@ -8,5 +8,13 @@ dataAnime = js.load(open(r"C:\Users\HP\Desktop\DBP\TAREAS\anime.json"))
 def getAnimes():
     return dataAnime
 
+@app.route("/anime/{<int:id_Anime>}")
+def getAnime(id_Anime):
+    for anime in dataAnime['anime']:
+        if anime['id'] == id_Anime:
+            return anime
+    return "No se encontro el anime :("
+
+
 if __name__ == "__main__":
     app.run(debug=True)
