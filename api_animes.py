c2fe30c2fc57a63cdf8da74c2f9b18b164436952
Jazmin Soto <jazmin.soto@utec.edu.pe>
2023-08-21 23:20:00 -0500

    Add method post (con filtrado)

--- api_animes.py
+++ api_animes.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import json as js
 
 app = Flask(__name__)
@@ -15,6 +15,21 @@ def getAnime(id_Anime):
             return anime
     return "No se encontro el anime :("
 
+@app.route("/anime", methods= ['POST'])
+def addAnime():
+    anime = { #Que solo aceptara este formato y lo que el cliente añada de mas no lo tomara en cuenta
+        "id": request.json['id'],
+        "titulo": request.json['titulo'],
+        "puntaje": request.json['puntaje'],
+        "tipo": request.json['tipo'],
+        "season": request.json['season'],
+        "generos": request.json['generos'],
+    }
+    dataAnime['anime'].append(anime)
+    with open(r"C:\Users\HP\Desktop\DBP\TAREAS\anime.json", 'w') as archivo: #Abriendo archivo json para escritura
+        js.dump(dataAnime, archivo)
+    
+    return dataAnime
 
 if __name__ == "__main__":
     app.run(debug=True)
